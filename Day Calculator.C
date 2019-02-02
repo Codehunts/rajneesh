00c54c8a02012ef531d5cf48424d15ce0130ab2d
rajnishdascse <45910029+rajnishdascse@users.noreply.github.com>
2019-02-02 15:36:57 +0530

    Day Calculator
    This caculator developped for Calculate Days

---
+++ Day Calculator.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a,days,months;
+ clrscr();
+ printf("Enter days");
+ scanf("%d",&a);
+ months=a/30;
+ days=a%30;
+
+ printf("%d months %d days",months,days);
+ getch();
+
+ }
