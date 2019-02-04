6ea69e7f94f87556c29c8d3d1a015a997403d421
Mudit kashyap <muditkashyap786@gmail.com>
2019-02-03 18:40:15 -0800

    Updated Swap.C
    You make a little bit mistake that create a logical error in file. Now check code. If you any query regarding same. Ping me.

--- SWAP.C
+++ SWAP.C
@@ -1,13 +1,14 @@
 #include<stdio.h>
-#include<conio.h>
+
 int main()
 {
  int a=5, b=2;
  a=a+b;
  b=a-b;
-
- printf("%d",a);
- printf("%d",b);
+ a=a-b;
+printf("Value After Swap:\n");
+ printf("Value of a=%d\n",a);
+ printf("Value of b=%d\n",b);
  return 0;
  }
 
